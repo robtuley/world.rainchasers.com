f7624282190f01bda609a3306bc7844bb11fef6b
Rob Tuley <rob@openknot.com>
2010-10-06 23:57:57 +0100

    Setup initial concept page

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Find Your Next Swim</title>
+  <link rel=stylesheet type=text/css href=css/screen.css>
+</head>
+
+<body>
+
+<nav>
+<ul>
+  <li><a href="./">Season Chart</a></li>
+  <li><a href=bc>British Columbia</a></li>
+  <li><a href=california>California</a></li>
+  <li><a href=cevennes>Cevennes</a></li>
+  <li><a href=chile>Chile</a></li>
+  <li><a href=french-alps>French Alps</a></li>
+  <li><a href=greece>Greece</a></li>
+  <li><a href=iran>Iran</a></li>
+  <li><a href=kyrgyzstan>Kyrgyzstan</a></li>
+  <li><a href=ladakh>Ladakh</a></li>
+  <li><a href=morocco>Morocco</a></li>
+  <li><a href=nepal>Nepal</a></li>
+  <li><a href=norway>Norway</a></li>
+  <li><a href=peru>Peru</a></li>
+  <li><a href=portugal>Portugal</a></li>
+  <li><a href=pyrenees>Pyrenees</a></li>
+  <li><a href=scotland>Scotland</a></li>
+  <li><a href=siberia>Siberia</a></li>
+  <li><a href=slovenia>Slovenia</a></li>
+  <li><a href=val-sesia>Val Sesia</a></li>
+</ul>
+</nav>
+
+<article>
+<h1>Whitewater Kayaking Seasons</h1>
+
+<p>nextsw.im helps you find your next kayaking adventure with concise summaries of
+   world kayaking destinations and seasons.</p>
+
+<!-- @todo season table -->
+
+</article>
+
+</body>
+</html>
