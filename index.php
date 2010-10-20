16e948c80a9f8944cbac5e4540991a6f7171aa47
Rob Tuley <rob@openknot.com>
2010-10-21 00:00:23 +0100

    Added facebook integration

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 /**
  * nextsw.im front controller.
+ *  FB-APP-ID 159877897376773
+ *  FB-APP-SECRET f5f89f1471b8aa64bdb3392a67f31cb9
  */
 header('Content-type: text/html; charset=utf-8');
-header('X-Frame-Options: DENY'); // don't steal my content
+header('X-Frame-Options: DENY');
+$escape = function($val) {
+    return htmlentities($val,ENT_COMPAT,'UTF-8');
+};
+
 $src = isset($_GET['src']) ? $_GET['src'] : null;
 if ($src) {
     // for src to be valid, it must only consist of a-z or dash, and it must
@@ -15,23 +21,39 @@ if ($src) {
     if ($path && file_exists($path)) {
         require_once __DIR__.'/lib/markdown.php';
         $content = Markdown(file_get_contents($path));
-        $title = 'Kayaking in '.implode(' ',array_map('ucfirst',explode('-',$src)));
+        $fb_title = implode(' ',array_map('ucfirst',explode('-',$src)));
+        $title = 'Kayaking in '.$fb_title;
+        $fb_type = 'country';
     } else {
         // 404, file does not exist
         header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
         $content = file_get_contents(__DIR__.'/src/404.html');
-        $title = 'Page Not Found';
+        $title = $fb_title = 'Page Not Found';
+        $fb_type = 'website';
     }
 } else {
     $content = file_get_contents(__DIR__.'/src/seasons.html');
-    $title = 'Kayaking Seasons';
+    $title = $fb_title = 'Kayaking Seasons';
+    $fb_type = 'website';
 }
+$fb_url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$fb_img = strlen($_SERVER['REQUEST_URI'])<=1 ? '/website' : $_SERVER['REQUEST_URI'];
+  $fb_img = '/website'; // temporary until populate fb images
+$fb_img = 'http://'.$_SERVER['HTTP_HOST'].'/fb'.$fb_img.'.png';
 ?>
 <!DOCTYPE html>
-<html lang=en>
+<html lang=en xmlns=http://www.w3.org/1999/xhtml
+      xmlns:og=http://opengraphprotocol.org/schema/
+      xmlns:fb=http://www.facebook.com/2008/fbml>
 <head>
-  <title><?php echo htmlentities($title,ENT_COMPAT,'UTF-8'); ?></title>
+  <title><?php echo $escape($title); ?></title>
   <meta charset=utf-8>
+  <meta property=og:title content="<?php echo $escape($fb_title); ?>"> 
+  <meta property=og:type content="<?php echo $escape($fb_type); ?>"> 
+  <meta property=og:url content=<?php echo $escape($fb_url); ?>> 
+  <meta property=og:image content=<?php echo $escape($fb_img); ?>> 
+  <meta property=fb:app_id content=159877897376773> 
+  <meta property=og:site_name content=nextsw.im> 
   <link rel=stylesheet media=screen,projection href=css/screen.css>
   <link rel=stylesheet media=print href=css/print.css>
   <script src=js/jquery.js></script>
@@ -41,7 +63,16 @@ if ($src) {
 
 <body>
 
+<article>
+<?php echo $content; ?>
+</article>
+
+<aside>
+<fb:comments width=750></fb:comments>
+</aside>
+
 <nav>
+<fb:like layout=button_count width=160></fb:like>
 <ul>
   <li class=first><a href=./>Season Chart</a></li>
 
@@ -105,9 +136,21 @@ if ($src) {
 $(function(){ $('nav li:not(:has(a))').hide(); });
 </script>
 
-<article>
-<?php echo $content; ?>
-</article>
+<div id=fb-root></div>
+<script>
+window.fbAsyncInit = function() {
+  FB.init({appId: '159877897376773', status: true, cookie: true,
+           xfbml: true});
+};
+(function() {
+  var e = document.createElement('script');
+  e.type = 'text/javascript';
+  e.src = document.location.protocol +
+       '//connect.facebook.net/en_GB/all.js';
+  e.async = true;
+  document.getElementById('fb-root').appendChild(e);
+}());
+</script>
 
 </body>
 </html>
