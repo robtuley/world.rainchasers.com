3db7dfe939206ef80c01b9c5fa83824c52c792bb
Rob Tuley <rob@openknot.com>
2010-10-25 18:02:44 +0100

    Added season chart source and two-tone season indication

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ $fb_img = 'http://'.$_SERVER['HTTP_HOST'].'/fb'.$fb_img.'.png';
 
 <body>
 
-<article>
+<article class=clearfix>
 <?php echo $content; ?>
 </article>
 
@@ -83,60 +83,60 @@ $fb_img = 'http://'.$_SERVER['HTTP_HOST'].'/fb'.$fb_img.'.png';
   <li class=first><a href=./>Season Chart</a></li>
 
   <li><span data-season=6,7>Alaska</span></li>
-  <li><a href=altai title=Siberia data-season=7,8>Altai</a></li>
-  <li><span data-season=8>Argentina</span></li>
-  <li><span data-season=5,6,7,8>Austria</span></li>
+  <li><a href=altai title=Siberia data-season=6:high,7,8 data-state=rain>Altai</a></li>
+  <!--<li><span data-season=8>Argentina</span></li>-->
+  <li><span data-season=5,6,7,8 data-state=snow>Austria</span></li>
   <!--<li>Bolivia</li>-->
   <!--<li>Brazil</li>-->
-  <li><span title=Canada data-season=6,7,8>British Columbia</span></li>
+  <li><span title=Canada data-season=6,7,8,9:low data-state=snow>British Columbia</span></li>
   <li><a href=california title=USA data-season=3,4,5,6,7>California</a></li>
-  <li><a href=cevennes title=France data-season=4,5,10,11>Cévennes</a></li>
-  <li><a href=chile data-season=1,2,3,12>Chile</a></li>
+  <li><a href=cevennes title=France data-season=4,5,10,11 data-state=rain>Cévennes</a></li>
+  <li><a href=chile data-season=1,2,3,11:high,12>Chile</a></li>
   <!--<li>Columbia</li>-->
-  <li><span data-season=4>Corsica</span></li>
+  <li><span data-season=4 data-state=snow>Corsica</span></li>
   <li><span title=Turkey data-season=5,6,7,8>Coruh</span></li>
   <li><a href=costa-rica data-season=6,7,8,9,10,11,12>Costa Rica</a></li>
   <li><span data-season=8,9>Cuba</span></li>
   <!--<li>Ecuador</li>-->
   <!--<li>Galicia</li>-->
   <li><span data-season=5,6,7,8,9,10>Georgia</span></li>
-  <li><a href=greece data-season=4,5>Greece</a></li>
+  <li><a href=greece data-season=4,5 data-state=snow>Greece</a></li>
   <li><span data-season=7,8>Greenland</a></li>
-  <li><span title=France data-season=5,6,7>Haut Alps</span></li>
+  <li><span title=France data-season=5,6,7:low,8:low data-state=snow>Haut Alps</span></li>
   <li><span data-season=4,5,6>Iceland</span></li>
   <li><span data-season=4,5,6>Idaho</span></li>
   <li><span data-season=5,6>Iran</span></li>
-  <li><a href=ireland data-season=1,2,3,4,10,11,12>Ireland</a></li>
+  <li><a href=ireland data-season=1:low,2,3,4,10,11,12:low data-state=rain>Ireland</a></li>
   <li><a href=japan data-season=7,8>Japan</a></li>
   <!--<li>Kamchatka</li>-->
   <!--<li>Korea</li>-->
-  <li><a href=kyrgyzstan data-season=6,7,8,9>Kyrgyzstan</a></li>
+  <li><a href=kyrgyzstan data-season=6,7,8,9:low data-state=snow>Kyrgyzstan</a></li>
   <!--<li>Kurdistan</li>-->
-  <li><span title=India data-season=8>Ladakh</span></li>
+  <li><span title=India data-season=8:snow,9:low>Ladakh</span></li>
   <!--<li>Madagascar</li>-->
   <li><span data-season=1,2,3,11,12>Mexico</span></li>
   <li><span data-season=6,7>Mongolia</span></li>
-  <li><a href=morocco data-season=4,5>Morocco</a></li>
-  <li><a href=nepal data-season=3,4,10,11,12>Nepal</a></li>
+  <li><a href=morocco data-season=4,5 data-state=snow>Morocco</a></li>
+  <li><a href=nepal data-season=3,4,5,9:high,10,11,12:low>Nepal</a></li>
   <li><a href=new-zealand data-season=1,2,3,4,11,12>New Zealand</a></li>
-  <li><a href=norway data-season=5,6,7,8>Norway</a></li>
+  <li><a href=norway data-season=5,6,7,8,9:low data-state=snow>Norway</a></li>
   <li><span data-season=4,5,6>Oregon</span></li>
   <li><span data-season=10,11>Panama</span></li>
   <!--<li>Papua New Guinea</li>-->
-  <li><a href=peru data-season=2,3,4,5,6,7,8>Peru</a></li>
-  <li><a href=portugal data-season=1,2,3,4,11,12>Portugal</a></li>
+  <li><a href=peru data-season=2:high,3,4,5,6,7,8,9:low data-state=snow>Peru</a></li>
+  <li><a href=portugal data-season=1:low,2,3,4:low,11:low,12:low data-state=rain>Portugal</a></li>
   <li><a href=putorana title=Siberia data-season=7>Putorana</a></li>
-  <li><a href=pyrenees title=Spain/France data-season=4,5>Pyrénées</a></li>
+  <li><a href=pyrenees title=Spain/France data-season=4,5 data-state=snow>Pyrénées</a></li>
   <li><span data-season=5,6,7,8,9>Quebec</span></li>
   <li><a href=sayan title=Siberia data-season=7,8>Sayan</a></li>
-  <li><span data-season=4,5,6,7>Slovenia</span></li>
+  <li><span data-season=4,5,6,7 data-state=snow>Slovenia</span></li>
   <!--<li>South Africa</li>-->
   <!--<li>Sweden</li>-->
   <li><span data-season=5,6>Taiwan</span></li>
   <li><span data-season=9,10,11>Tajikistan</span></li>
-  <li><span data-season=1,2,3,4,5,6,7,8,9,10,11,12>Uganda</span></li>
-  <li><span data-season=1,2,3,4,10,11,12>UK</span></li>
-  <li><a href=val-sesia title=Italy data-season=4,5>Val Sesia</a></li>
+  <li><span data-season=1,2,3,4,5,6,7,8,9,10,11,12 data-state=dam>Uganda</span></li>
+  <li><span data-season=1:low,2,3,4,10,11,12:low data-state=rain>UK</span></li>
+  <li><a href=val-sesia title=Italy data-season=4,5 data-state=snow>Val Sesia</a></li>
   <!--<li>Vietnam</li>-->
   <li><span data-season=4,5,6>Washington</span></li>
   <li><span data-season=9,10,11>Zambezi</a></li>
